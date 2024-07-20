9cd792f9519e8d15fcacef28dc083a7e85f433b8
osaro123 <osarolawani983@gmail.com>
2024-07-20 22:08:29 +0100

    Add feature section

--- components/LogoTicker.tsx
+++ components/LogoTicker.tsx
@@ -21,7 +21,7 @@ const LogoTicker = () => {
   return (
     <div className="bg-black text-white py-[72px] sm:py-24">
         <div className="container">
-            <h2 className="text-center text-xl text-white/70">Trusted by the world's most innovative teams</h2>
+            <h2 className="text-center text-xl text-white/70">Trusted by the world&apos;s most innovative teams</h2>
             <div className='overflow-hidden mt-9 before:content-[""] after:content-[""] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))] before:left-0 after:right-0 before:top-0 after:top-0'>
                 <div className="flex gap-16">
                     {images.map(({src,alt},i) => (
