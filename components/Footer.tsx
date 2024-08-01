6cf8b97f24ed6c5063358675dff5aabdb7c3c9be
osaro123 <osarolawani983@gmail.com>
2024-08-01 23:30:13 +0100

    Add footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import InstaIcon from "../assets/icons/insta.svg"
+import XSocialIcon from "../assets/icons/x-social.svg"
+import TiktokIcon from "../assets/icons/tiktok.svg"
+import YoutubeIcon from "../assets/icons/youtube.svg"
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className='py-5 bg-black text-white/60'>
         <div className='container'>
-            <div>
-                <div>@ 2024 Osaro Lawani, Inc. All rights reserved</div>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+            <div className="flex flex-col gap-4 sm:flex-row sm:justify-between">
+                <div className="text-center">@ 2024 Osaro Lawani, Inc. All rights reserved</div>
+                <ul className="flex justify-center gap-2.5">
+                    <li>
+                        <InstaIcon/>
+                    </li>
+                    <li>
+                        <XSocialIcon/>
+                    </li>
+                    <li>
+                        <TiktokIcon/>
+                    </li>
+                    <li>
+                        <YoutubeIcon/>
+                    </li>
                 </ul>
             </div>
         </div>
